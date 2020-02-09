052b99fffe07c5ebfe29116f104194c7fd701a34
Dmytro Pastukh <pastukh.dm@gmail.com>
2020-02-09 19:52:46 +0100

    Add ability to remove and update entries

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,152 @@
-import React from 'react';
-// import './App.scss';
-import { Container, TableContainer, Paper, TableHead, TableCell, Table, Button, TableBody, TableRow } from '@material-ui/core';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addEntry } from './store/entries/entriesSlice';
+import { createEntry, deleteEntry, updateEntry } from './store/entries/entriesSlice';
 import { RootState } from './store/RootState';
 import { Entry } from './store/entries/entriesState';
-// import moment from 'moment';
+import { MdDelete } from 'react-icons/md';
+import './App.scss';
+import { Container, Button, Table, Modal } from 'react-bootstrap';
+import DatePicker from "react-datepicker";
+import moment from 'moment';
+import en from "date-fns/locale/en-GB";
 
 const App = () => {
   const dispatch = useDispatch();
   const entries = useSelector<RootState, Entry[]>(state => state.entries.list);
+
+  const handleAddEntry = () => dispatch(createEntry('New entry'))
+  const handleDeleteEntry = (entry: Entry) => dispatch(deleteEntry(entry.id))
+  const handleUpdateEntry = (entry: Entry) => dispatch(updateEntry(entry))
+
   return (
     <div className="App">
       <Container>
+        <br />
         <h1>Spaced Repetition Tracker</h1>
+        <br />
         <Button
-          variant="contained"
-          color="primary"
-          onClick={() => dispatch(addEntry('New entry'))}
+          variant="primary"
+          onClick={handleAddEntry}
         >
           Add
-          </Button>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Title</TableCell>
-                <TableCell>Learned at</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {
-                entries.map(entry =>
-                  <TableRow key={entry.id}>
-                    <TableCell>{entry.title}</TableCell>
-                  
-                    <TableCell>{entry.learnedAt}</TableCell>
-                  </TableRow>
-                )
-              }
-            </TableBody>
-          </Table>
-        </TableContainer>
+        </Button>
+        <br />
+        <br />
+        {
+          entries.length > 0 ?
+            <Table>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Learned at</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  entries.map(entry =>
+                    <EntryRow
+                      key={entry.id}
+                      entry={entry}
+                      onDelete={handleDeleteEntry}
+                      onUpdate={handleUpdateEntry}
+                    />
+                  )
+                }
+              </tbody>
+            </Table>
+            : 'No entries'
+        }
       </Container>
     </div>
   );
+}
+
+const EntryRow = ({ entry, onDelete, onUpdate }: EntryRowProps) => {
+  const [show, setShow] = useState(false);
+  const [startDate, setStartDate] = useState(moment(entry.learnedAt, 'DD.MM.YYYY').toDate());
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleDelete = () => {
+    onDelete(entry);
+    setShow(false);
+  };
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.which === 13) {
+      e.preventDefault();
+    }
+  }
+  const handleTitleChange = (e: React.FocusEvent<HTMLInputElement>) => {
+    const nextTitle = e.target.value || entry.title;
+    if (nextTitle !== entry.title) {
+      onUpdate({ ...entry, title: nextTitle });
+    }
+  }
+  const handleLearnedAtChange = (date: Date) => {
+    const nextLearnedAt = moment(date).format('DD.MM.YYYY');
+    if (nextLearnedAt !== entry.learnedAt) {
+      onUpdate({ ...entry, learnedAt: nextLearnedAt });
+    }
+    setStartDate(date);
+  }
+
+  return (
+    <tr key={entry.id}>
+      <td>
+        <input
+          type="text"
+          defaultValue={entry.title}
+          onKeyDown={handleKeyDown}
+          onBlur={handleTitleChange}
+        />
+      </td>
+      <td>
+        <DatePicker
+          selected={startDate}
+          onChange={handleLearnedAtChange}
+          locale={en}
+          dateFormat="dd.MM.yyyy"
+          todayButton="Today"
+        />
+      </td>
+      <td>
+        <button
+          className="text-danger"
+          onClick={handleShow}
+        >
+          <MdDelete />
+        </button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            You are going to delete entry: "{entry.title}"
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="danger"
+              onClick={handleDelete}
+            >
+              Delete
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </td>
+    </tr>
+  )
+}
+interface EntryRowProps {
+  entry: Entry,
+  onDelete: (entry: Entry) => void
+  onUpdate: (entry: Entry) => void
 }
 
 export default App;
--- src/store/entries/entriesSlice.ts
+++ src/store/entries/entriesSlice.ts
@@ -1,4 +1,4 @@
-import { entriesState } from './entriesState';
+import { entriesState, Entry } from './entriesState';
 import { createSlice } from '@reduxjs/toolkit';
 import moment from 'moment';
 const uuidv1 = require('uuid/v1');
@@ -7,9 +7,16 @@ export const entriesSlice = createSlice({
   name: 'entries',
   initialState: entriesState,
   reducers: {
-    addEntry(state, action: { payload: string }) {
+    createEntry(state, action: { payload: string }) {
       state.list.push({ id: uuidv1(), title: action.payload, learnedAt: moment().format('DD.MM.YYYY'), repeatedAt: [] });
     },
+    deleteEntry(state, action: { payload: string }) {
+      state.list = state.list.filter(entry => entry.id !== action.payload)
+    },
+    updateEntry(state, action: { payload: Entry }) {
+      const itemIndex = state.list.findIndex(item => item.id === action.payload.id);
+      state.list[itemIndex] = action.payload;
+    }
   }
 });
-export const { addEntry } = entriesSlice.actions;
+export const { createEntry, deleteEntry, updateEntry } = entriesSlice.actions;
